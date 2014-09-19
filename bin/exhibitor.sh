8dfe01efbe461dde2cd5d73632b2d2622e2bfdd5
Adam Davis <adavis@mdsol.com>
2014-09-19 15:54:02 +0100

    fix start script for 12-factor

--- bin/exhibitor.sh
+++ bin/exhibitor.sh
@@ -8,21 +8,47 @@ if test -f $CONFIG_HOME/exhibitor.cfg; then
   . $CONFIG_HOME/exhibitor.cfg
 fi
 
-case "$1" in
-  start)
-        if [ -f "/usr/bin/ec2metadata" ] ; then
+start() {
+	 if [ -f "/usr/bin/ec2metadata" ] ; then
             OS_HOSTNAME=$(ec2metadata --local-hostname)
         else
             OS_HOSTNAME=$(hostname -f)
             fi
         cd /mnt/zookeeper/current/bin
-        exec java -Dlog4j.configuration=file:///${CONFIG_HOME}/log4j.properties -jar ${EXHIBITOR_HOME}/${EXHIBITOR_LIB} --hostname ${OS_HOSTNAME} ${EXHIBITOR_OPTS} > exhibitor.out &
+        running=`ps -ef |grep java |grep exh |grep -v grep |tr -s " "|cut -d " " -f2|wc -l`
+
+        if [ "$running"  -ge 1 ]
+        then
+                 echo " process is already running ";
+                exit;
+
+        else
+
+        exec java -Dlog4j.configuration=file:///${CONFIG_HOME}/log4j.properties -jar ${EXHIBITOR_HOME}/${EXHIBITOR_LIB} --hostname ${OS_HOSTNAME} ${EXHIBITOR_OPTS} > exhibitor.out 
+	##add a i"&"  on the end of the line above if using as a conventional init script. removed as 12-factor wants to run the process in the forground.  
+        fi
+}
+
+stop() {
+       #kill pid
+        kill `ps -ef |grep java |grep exhibitor |grep -v grep |tr -s " "|cut -d " " -f2`
+        echo "Process Killed"
+}
+
+
+case "$1" in
+  start)
+	start	
         ;;
   stop)
-	#kill pid 
-  ;;
+	stop
+	;;
+  restart|reload)
+	 stop
+        start
+        ;;
 *)
-        echo "Usage: /etc/init.d/ssh {start|stop|reload|force-reload|restart|try-restart|status}" || true
+        echo "Usage: {start|stop|reload|restart|status}" || true
         exit 1
 esac
 
